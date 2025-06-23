fa07e933b8d1875d7ba98fd6d499e2125f660552
olorunwa-web <olorunwamoliki@gmail.com>
2025-06-23 12:33:41 +0100

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
                 <section className = 'container mx-auto px-4 md:px-8'>
                     <div className = 'flex flex-col gap-y-6'>
                         <div className = ' w-full md:w-[600px] lg:w-[770px] xl:w-[700px]'>
-                            <h1 className = 'font-bold text-[1.75rem] md:text-[40px] lg:text-[2.8rem] xl:text-[3.1rem] text-white leading-[40px] md:leading-[57px] lg:leading-[64px] xl:leading-[74px] '>Utilization of Technology to Empower Farmers Across Africa Agriculture</h1>
+                            <h1 className = 'font-bold text-[1.75rem] md:text-[40px] lg:text-[2.8rem] xl:text-[3.1rem] text-white leading-[40px] md:leading-[57px] lg:leading-[64px] xl:leading-[69px] '>Utilization of Technology to Empower Farmers Across Africa Agriculture</h1>
                         </div>
                         <div>
                             <button className = 'md:h-[55px] h-[55px] bg-[#4CAF50] rounded-[56px] px-[23px] md:px-[23px] py-3 text-base font-semibold text-white '>Apply for Funding</button>
