4bd5b66472cf8187c3577bfd4e9ca631c05f69ce
olorunwa-web <olorunwamoliki@gmail.com>
2025-06-25 03:11:22 +0100

    first commit

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -6,7 +6,7 @@ import Swipes2 from '../components/Swipes2';
 const Testimonial = () => {
     return (
         <>
-             <section className = 'mt-14 mb-7'>
+             <section className = 'mt-15 mb-7'>
                 <p className = 'text-center font-normal text-[#808080] text-base mb-1'>WHAT THEY SAY</p>
                 <h1 className = 'text-center text-[1.4rem] md:text-[1.7rem] lg:text-[1.7rem] font-bold mb-4 text-[#0D0D0D]'>Testimonial</h1>
                 <section>
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import contactback from '../assets/image 1.png'
+
+
+
 
 const ContactUs = () => {
     return (
         <>
-            contactus
+             <section className ="bg-cover no py-28 lg:py-35 xl:py-50 h-full max-h-full" style = {{ backgroundImage: `url(${contactback})`}}>
+                <section className = 'w-11/12 max-w-8xl mx-auto'>
+                    <h1 className = 'mb-3 font-bold text-white text-[2.2rem] md:text-[2.7rem] lg:text-[3.2rem] xl:text-[3.7rem]'>Contact Us</h1>
+                    <p className = ' w-full md:w-[650px] lg:w-[700px] font-normal text-white text-[1.1rem] md:text-[1.2rem] lg:text-[1.3rem]'>We'd love to hear from you!</p>
+                    <p className = ' w-full md:w-[650px] lg:w-[700px] font-normal text-white text-[1.1rem] md:text-[1.2rem] lg:text-[1.3rem]'>Whether you have questions about our services, need support, or just want to share your feedback - we're hereto help.</p>
+                </section>
+            </section>
         </>
     )
 }
