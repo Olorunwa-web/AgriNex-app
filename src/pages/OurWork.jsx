7a598f8da55d6c1e1c2a9ad174513c5cbf78f81c
olorunwa-web <olorunwamoliki@gmail.com>
2025-06-25 01:14:26 +0100

    first commit

--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -8,7 +8,7 @@ import Prefoot from '../components/Prefooter';
 const OurWork = () => {
     return (
         <>
-            <section className ="bg-cover py-28  lg:py-35 xl:py-43 h-full max-h-full" style = {{ backgroundImage: `url(${workground})`}}>
+            <section className ="bg-cover py-28  lg:py-35 xl:py-50 h-full max-h-full" style = {{ backgroundImage: `url(${workground})`}}>
                 <section className = 'w-11/12 max-w-8xl mx-auto'>
                     <h1 className = 'mb-3 font-bold text-white text-[2.2rem] md:text-[2.7rem] lg:text-[3.2rem] xl:text-[3.7rem]'>Our Work</h1>
                     <p className = ' w-full md:w-[650px] lg:w-[700px] font-normal text-white text-[1.1rem] md:text-[1.2rem] lg:text-[1.3rem]'>Empowering farmers, transforming agriculture, and building sustainable futures-because technology alone doesn't change lives, people do. We're here to support the hands that feed the world.</p>
